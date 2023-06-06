1b7e85024face94714e1ea1c21a4412da1637cb8
Kyle Roth <kylrth@gmail.com>
2023-06-06 17:30:43 +0000

    fix eval message creation bug
    We were accidentally setting the value in the global variable the first
    time we called each eval function, and then using that same string for
    each following call.

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -13,70 +13,77 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 evaluation_messages = {
-    "ingredient_comparison": [
-        SystemMessage(
-            content="You are a evaluating two ingredients lists to see how well the first lists' "
+    "ingredient_comparison": {
+        "system": (
+            "You are a evaluating two ingredients lists to see how well the first lists' "
             "ingredients match the second. Count how many ingredients are used in "
-            "both lists, the wording or measurements don't have to be exactly the same as long as"
+            "both lists, the wording or measurements don't have to be exactly the same as long as "
             "the ingredient is the same material. "
-            "For example: There are 5 matching ingredients between the two lists: eggs, flour,"
-            "vanilla extract, milk, and butter.; There are 2 matching ingredients between the two"
+            "For example: There are 5 matching ingredients between the two lists: eggs, flour, "
+            "vanilla extract, milk, and butter.; There are 2 matching ingredients between the two "
             "lists: peanuts and jelly; There are 0 matching ingredients between the two lists"
         ),
-        HumanMessage(
-            content=(
-                "Here are the two ingredients' lists : ---LIST 1 START---\n{recipeIngredients}\n"
-                "---LIST 1 END---\n---LIST 2 START---\n{goldIngredients}\n---LIST 2 END---\n"
-            )
+        "human": (
+            "Here are the two ingredients' lists : ---LIST 1 START---\n{recipeIngredients}\n"
+            "---LIST 1 END---\n---LIST 2 START---\n{goldIngredients}\n---LIST 2 END---\n"
         ),
-    ],
-    "ingredient_consistency": [
-        SystemMessage(
-            content=(
-                "You are evaluating recipes to count the number of inconsistencies between the "
-                "steps and the ingredient list. An inconsistency is using an ingredient in the "
-                "steps that is not mentioned in the ingredients' list, or not using the same "
-                "amount of an ingredient as mentioned in the ingredients' list, etc... Start your "
-                "response by stating the number of inconsistencies that there are. For example: "
-                "There are 0 inconsistencies; There is 1 inconsistency; There are 2 "
-                "inconsistencies; There are 4 inconsistencies; etc..."
-            )
+    },
+    "ingredient_consistency": {
+        "system": (
+            "You are evaluating recipes to count the number of inconsistencies between the steps "
+            "and the ingredient list. An inconsistency is using an ingredient in the steps that is "
+            "not mentioned in the ingredients' list, or not using the same amount of an ingredient "
+            "as mentioned in the ingredients' list, etc... Start your response by stating the "
+            "number of inconsistencies that there are. For example: There are 0 inconsistencies; "
+            "There is 1 inconsistency; There are 2 inconsistencies; There are 4 inconsistencies; "
+            "etc..."
         ),
-        HumanMessage(content="Evaluate this recipe :\n{recipe}"),
-    ],
-    "step_order": [
-        SystemMessage(
-            content="You are a evaluating recipes to make sure that the steps are in the correct, "
-            "cohesive order. The answer is True if the steps are in the correct order, and False "
-            "if the steps are in incorrect order. Start your answer by stating your answer: True "
-            "or False, followed by your justification. For example: True. The recipe's steps are "
-            "in a logical order; False. Step 3 should come after Step 4 because...; etc..."
+        "human": "Evaluate this recipe :\n{recipe}",
+    },
+    "step_order": {
+        "system": (
+            "You are a evaluating recipes to make sure that the steps are in the correct, cohesive "
+            "order. The answer is True if the steps are in the correct order, and False if the "
+            "steps are in incorrect order. Start your answer by stating your answer: True or "
+            "False, followed by your justification. For example: True. The recipe's steps are in a "
+            "logical order; False. Step 3 should come after Step 4 because...; etc..."
         ),
-        HumanMessage(content="Evaluate this recipe :\n{recipe}"),
-    ],
-    "coherence": [
-        SystemMessage(
-            content="You are a evaluating recipes to make sure that the recipe is coherent, clear "
-            "and readable. You will give the recipe a score out of 10. Start your output with the "
+        "human": "Evaluate this recipe :\n{recipe}",
+    },
+    "coherence": {
+        "system": (
+            "You are a evaluating recipes to make sure that the recipe is coherent, clear and "
+            "readable. You will give the recipe a score out of 10. Start your output with the "
             "score, then follow up with your justifications. For example : 10/10. The recipe is "
             "perfectly coherent; 9/10. The recipe is well-structured but has a minor "
             "imperfection...; etc..."
         ),
-        HumanMessage(content="Evaluate this recipe :\n{recipe}"),
-    ],
-    "ingredient_relevance": [
-        SystemMessage(
-            content="You are a evaluating recipes to make sure that the ingredients and steps are "
-            "in alignment with the title. Do the ingredients and recipe produce the dish in the "
-            "title and are culinary conditions respected. The answer is True if there is alignment "
-            "and False if not, followed by your explanation. For example: False. The recipe title "
+        "human": "Evaluate this recipe :\n{recipe}",
+    },
+    "ingredient_relevance": {
+        "system": (
+            "You are a evaluating recipes to make sure that the ingredients and steps are in "
+            "alignment with the title. Do the ingredients and recipe produce the dish in the title "
+            "and are culinary conditions respected. The answer is True if there is alignment and "
+            "False if not, followed by your explanation. For example: False. The recipe title "
             "indicates that it is gluten-free but some ingredients have gluten; True. The "
             "ingredients align with the culinary expectations of the recipe; False. The recipe "
             "title indicates that it is no-bake but the steps include a baking step; etc..."
         ),
-        HumanMessage(content="Evaluate this recipe :\n{recipe}"),
-    ],
+        "human": "Evaluate this recipe :\n{recipe}",
+    },
 }
+
+
+def format_message_history(key: str, **kwargs):
+    messages = evaluation_messages[key]
+
+    return [
+        SystemMessage(content=messages["system"].format(**kwargs)),
+        HumanMessage(content=messages["human"].format(**kwargs)),
+    ]
+
+
 rouge_metric = evaluate.load("rouge")
 bleu_metric = evaluate.load("bleu")
 chatgpt = ChatOpenAI()
@@ -143,9 +150,10 @@ async def ingredient_comparison(recipe: str, gold: str, logger: logging.Logger) 
     recipeIngredients = recipe[recipeStartIdx:recipeEndIdx]
     goldIngredients = gold[goldStartIdx:goldEndIdx]
 
-    messages = evaluation_messages["ingredient_comparison"]
-    messages[1].content = messages[1].content.format(
-        recipeIngredients=recipeIngredients, goldIngredients=goldIngredients
+    messages = format_message_history(
+        "ingredient_comparison",
+        recipeIngredients=recipeIngredients,
+        goldIngredients=goldIngredients,
     )
 
     resp = await chatgpt.agenerate(messages=[messages])
@@ -165,9 +173,7 @@ async def ingredient_consistency(recipe: str, logger: logging.Logger) -> int:
     """Model Based Evaluation
     Does the ingredients' list accurately reflect the exact ingredients and amounts used in
     the directions according to an LLM?"""
-
-    messages = evaluation_messages["ingredient_consistency"]
-    messages[1].content = messages[1].content.format(recipe=recipe)
+    messages = format_message_history("ingredient_consistency", recipe=recipe)
 
     resp = await chatgpt.agenerate(messages=[messages])
     log_messages(logger, "ingredient_consistency prompt:\n", messages)
@@ -185,9 +191,7 @@ async def ingredient_consistency(recipe: str, logger: logging.Logger) -> int:
 async def step_order(recipe: str, logger: logging.Logger) -> bool:
     """Model Based Evaluation
     Does the order of the steps make sense according to an LLM?"""
-
-    messages = evaluation_messages["step_order"]
-    messages[1].content = messages[1].content.format(recipe=recipe)
+    messages = format_message_history("step_order", recipe=recipe)
 
     resp = await chatgpt.agenerate(messages=[messages])
     log_messages(logger, "step_order prompt:\n", messages)
@@ -203,9 +207,7 @@ async def step_order(recipe: str, logger: logging.Logger) -> bool:
 async def coherence(recipe: str, logger: logging.Logger) -> int:
     """Model Based Evaluation
     Is the recipe clear and readable according to an LLM?"""
-
-    messages = evaluation_messages["coherence"]
-    messages[1].content = messages[1].content.format(recipe=recipe)
+    messages = format_message_history("coherence", recipe=recipe)
 
     resp = await chatgpt.agenerate(messages=[messages])
     log_messages(logger, "coherence prompt:\n", messages)
@@ -223,9 +225,7 @@ async def ingredient_relevance(recipe: str, logger: logging.Logger) -> bool:
     Does the list of ingredients align with the culinary expectations of the recipe (e.g. no-bake,
     gluten-free...)?
     """
-
-    messages = evaluation_messages["ingredient_relevance"]
-    messages[1].content = messages[1].content.format(recipe=recipe)
+    messages = format_message_history("ingredient_relevance", recipe=recipe)
 
     resp = await chatgpt.agenerate(messages=[messages])
     log_messages(logger, "ingredient_relevance prompt:\n", messages)
