c655d249cb5d24ea58b6f9d2daf88e434cfc2c79
Kyle Roth <kylrth@gmail.com>
2024-01-31 02:44:20 +0000

    update black linter

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,7 +1,6 @@
 """This package provides the systems used in the paper. By "systems" we mean general approaches, not
 specific LMs."""
 
-
 from .aag import AAG
 from .few_shot import FewShot
 from .interface import Result, System
