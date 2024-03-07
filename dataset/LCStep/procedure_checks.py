fa86972f9df3cfd789f31f805d70b4f94aa0d09a
Kyle Roth <kylrth@gmail.com>
2024-03-07 21:30:38 +0000

    more fixes to procedure checks

--- dataset/LCStep/procedure_checks.py
+++ dataset/LCStep/procedure_checks.py
@@ -1,14 +1,62 @@
+# ruff: noqa: T201
+# This script needs to print.
+
 import argparse
 import asyncio
 import logging
+import os
+import subprocess
+import sys
+import tempfile
+from difflib import Differ
 from os import PathLike
 from pathlib import Path
+from typing import IO
 
 from langchain.chat_models import ChatOpenAI
 from langchain.chat_models.base import BaseChatModel
 from langchain.schema import BaseMessage, HumanMessage, SystemMessage
 
 from utils import spread_gather
+
+
+class Recorder:
+    """Keep track of which files we have already reviewed, so that we can pick up where we left off
+    if we exit and rerun the script."""
+
+    path: Path
+    f: IO[str] | None
+    done: set[str]
+
+    def __init__(self, path: str | PathLike):
+        self.path = Path(path)
+        self.f = None
+
+    def __enter__(self):
+        self.f = self.path.open("a+")
+        self.f.seek(0)
+        self.done = {line.strip() for line in self.f.readlines()}
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.f is not None:
+            self.f.close()
+
+    def __contains__(self, obj):
+        if self.f is None:
+            raise ValueError("inclusion check called outside of context")
+
+        return str(obj) in self.done
+
+    def add(self, file: str | PathLike):
+        if self.f is None:
+            raise ValueError("add method called outside of context")
+
+        file = str(file)
+
+        self.done.add(file)
+        self.f.write(file + "\n")
 
 
 def get_prompt_messages() -> list[BaseMessage]:
@@ -35,32 +83,94 @@ def make_logger(name: str) -> logging.Logger:
     return logger
 
 
+_editor = os.environ.get("VISUAL") or os.environ.get("EDITOR", "nano")
+
+
+def edit_text(text: str) -> str:
+    with tempfile.NamedTemporaryFile(suffix=".tmp", mode="w+") as f:
+        f.write(text)
+        f.flush()
+
+        subprocess.call([_editor, f.name])
+
+        f.seek(0)
+        return f.read()
+
+
+def diff(old: str, new: str) -> str:
+    d = Differ()
+
+    old_lines = old.splitlines(keepends=True)
+    new_lines = new.splitlines(keepends=True)
+
+    return "".join(
+        line[2:] if line.startswith("  ") else line for line in d.compare(old_lines, new_lines)
+    )
+
+
 async def judge_doc(
     model: BaseChatModel,
     prompt: list[BaseMessage],
     formatted_path: str | PathLike,
+    rec: Recorder,
 ):
     if "fixed" in str(formatted_path):
         return
 
     formatted_path = Path(formatted_path)
 
+    if formatted_path in rec:
+        print(f"skipped '{formatted_path}'", file=sys.stderr)
+
+        return
+
     messages = prompt.copy()
     with formatted_path.open() as f:
-        messages.append(HumanMessage(content=f.read().strip()))
+        text = f.read().strip()
+    messages.append(HumanMessage(content=text))
 
     resp = await model.agenerate(messages=[messages])
     judgement = resp.generations[0][0].text
-    if "[FAIL]" in judgement:
-        stem = formatted_path.stem
-        fixed_path = formatted_path.parent / (stem + "_fixed" + formatted_path.suffix)
-        logger = make_logger(stem)
-        logger.debug(judgement)
-        with fixed_path.open("w") as f:
-            if "Goal:" in judgement:
-                f.write(judgement[judgement.index("Goal:") :])
-            else:
-                f.write(judgement)
+
+    if "[FAIL]" not in judgement:
+        print(f"file '{formatted_path}' passed", file=sys.stderr)
+
+        rec.add(formatted_path)
+
+        return
+
+    logger = make_logger(formatted_path.stem)
+
+    print(f"file '{formatted_path}':", file=sys.stderr)
+    logger.debug("file: %s", formatted_path)
+    logger.debug("judgement: %s", judgement)
+
+    if "Goal:" in judgement:
+        criteria = judgement[: judgement.index("Goal:")].strip()
+        new_text = judgement[judgement.index("Goal:") :]
+
+        print("  criteria:\n" + criteria)
+        input("  press enter to edit the diff (leave blank to abort)")
+
+        modified = edit_text(diff(text, new_text))
+        if modified.strip() == "":
+            raise ValueError("user aborted by saving empty text")
+        with formatted_path.open("w") as f:
+            f.write(modified)
+        logger.debug("modified: %s", modified)
+
+        rec.add(formatted_path)
+        print("  done.")
+
+        return
+
+    print("  could not parse updated procedure from judgement", file=sys.stderr)
+    print("  judgement text:\n\n" + judgement, file=sys.stderr)
+    input("  press enter to edit the procedure manually")
+    subprocess.call([_editor, str(formatted_path)])
+
+    rec.add(formatted_path)
+    print("  done.")
 
 
 async def check_docs(root: str | PathLike, n: int, n_workers: int = 5):
@@ -79,12 +189,13 @@ async def check_docs(root: str | PathLike, n: int, n_workers: int = 5):
 
     n_workers = min(n_workers, len(files))
 
-    await spread_gather(
-        lambda fp: judge_doc(chatgpt, prompt, fp),
-        files,
-        n_workers,
-        len(files),
-    )
+    with Recorder("checks_logs/done.txt") as rec:
+        await spread_gather(
+            lambda fp: judge_doc(chatgpt, prompt, fp, rec),
+            files,
+            n_workers,
+            len(files),
+        )
 
 
 if __name__ == "__main__":
