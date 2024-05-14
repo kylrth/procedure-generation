03cdd996cb9e040b8e367a428bc5b5fbbff67b0e
Rushil-Gupta <rushil@LAPTOP-ILGT2KC7>
2024-05-14 11:43:06 -0400

    eval heuristics

---
+++ evaluation/api_overlap.py
@@ -0,0 +1,22 @@
+from dataset import Procedure
+
+def get_apis(response: str):
+    token_list = response.split('`')
+    start_idx = 1
+    api_list = []
+    while start_idx < len(token_list):
+        api_list.append(token_list[start_idx])
+        start_idx += 2
+    
+    return api_list
+
+def check_api_overlap(gold: Procedure, generated: str):
+    gold_apis = get_apis(str(gold.steps))
+    generated_apis = get_apis(generated)
+    
+    num_present = 0
+    for api in gold_apis:
+        if api in generated_apis:
+            num_present += 1
+    
+    return (num_present/len(gold_apis))
---
+++ evaluation/is_all_inp_used.py
@@ -0,0 +1,48 @@
+from dataset import Procedure
+from systems import Model
+from langchain.schema import BaseMessage, HumanMessage, SystemMessage
+import re
+
+def LCStep_all_resources_used(gold : Procedure, generated: str):
+    if gold._input == '':
+        return 1
+    
+    resource_str = gold._input
+    resource_str_list = resource_str.split(',')
+    
+    filtered_sentence = []
+    for w in resource_str_list:
+        w = w.lower()
+        if w.startswith("an"):
+            w = w[3:]
+        elif w.startswith("a"):
+            w = w[2:]
+        elif w.startswith("the"):
+            w = w[4:]
+            
+        filtered_sentence.append(w)
+        
+    resource_present = 0
+    
+    for resource_word in filtered_sentence:
+        if resource_word in generated:
+            resource_present += 1
+    
+    return (resource_present/len(filtered_sentence))
+
+
+def RecipeNLG_all_ingredients_used(model: Model, gold: Procedure, generated: str):
+    ing_list = gold._input    
+    prompt = [
+            SystemMessage(content="Please rephrase the ingredient list below keeping only the name of the ingredient as it would appear in a recipe. For example, 8 oz. penne or other tubular pasta becomes 'pasta', 1 1/2 c. frozen green beans becomes 'green beans', and 2 Tbsp. chopped flat leaf parsley becomes 'parsley'."),
+            HumanMessage(content=f"Referring to above examples, modify the list below:\n {str(ing_list)}\n Your response should be a python list starting with [ and end with ]. Remove the text enclosed in ()."),
+            ]
+    
+    trim_ing_list = model.generate(prompt)
+    list_items = re.sub(r'[\s]', '', trim_ing_list)[1:-1].split(',')
+    ing_found = 0
+    for ing in list_items:
+        if ing in generated:
+            ing_found += 1
+    
+    return (ing_found/len(list_items))
