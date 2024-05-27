fcb5142c711eed0f93b5cba60153776a29c517c9
Kyle Roth <kylrth@gmail.com>
2024-05-27 18:48:26 +0000

    reservoir sampling for RecipeNLG
    This dataset is large, so we do reservoir sampling to collect a
    representative sample of size n. Before, we were just taking the first
    n lines of the dataset file.

--- dataset/recipenlg.py
+++ dataset/recipenlg.py
@@ -1,9 +1,11 @@
 import csv
+import itertools
 import json
+import math
 import random
-from itertools import islice
+import sys
 from os import PathLike
-from typing import Any
+from typing import Any, Iterable
 
 from .base import Dataset, Doc, Procedure
 
@@ -17,22 +19,77 @@ def recipe_to_procedure(d: dict[str, Any]) -> Procedure:
 
 
 class RecipeNLG(Dataset):
-    def __init__(self, data_dir: str | PathLike, n: int | None = None):
+    """A random subset of size n of the RecipeNLG dataset."""
+
+    def __init__(self, data_dir: str | PathLike, n: int = sys.maxsize):
         super().__init__(data_dir)
-        self.n = n
-        self.rng = random.Random(42)
+        self.reservoir = Reservoir(n, seed=42)
 
     def _init_procedures(self) -> list[Procedure]:
-        out = []
         with (self.dir / "RecipeNLG" / "full_dataset.csv").open(newline="") as f:
-            reader = csv.DictReader(f)
-            for row in islice(reader, self.n):
-                out.append(recipe_to_procedure(row))
+            header = f.readline()
+            self.reservoir.sample(f)
 
-        self.rng.shuffle(out)
+        reader = csv.DictReader(itertools.chain((header,), self.reservoir.samples))
+        out = []
+        for row in reader:
+            out.append(recipe_to_procedure(row))
+
+        self.reservoir.rng.shuffle(out)
 
         return out
 
     def _get_docs(self) -> list[Doc]:
         # TODO get generic cooking material
         return []
+
+
+class Reservoir:
+    """Implements "algorithm L" for reservoir sampling as described here:
+    https://en.wikipedia.org/wiki/Reservoir_sampling#Optimal:_Algorithm_L
+
+    Closely follows what was written here:
+    https://github.com/alexprengere/reservoir/blob/master/reservoir.py
+    but with the nice interface from here:
+    https://github.com/mattiaciollaro/reservoir/blob/master/reservoir.py
+    """
+
+    size: int
+    seen: int
+    samples: list
+    rng: random.Random
+
+    def __init__(self, size: int, seed=None):
+        self.size = size
+        self.seen = 0
+        self.samples = []
+        self.rng = random.Random(seed)
+
+    def sample(self, i: Iterable):
+        """Performs reservoir sampling from i. Samples are placed in self.samples.
+
+        If this method is called multiple times, self.samples will be a uniformly random subset of
+        items from all these iterables.
+        """
+        gap_threshold = 4 * self.size
+
+        iterator = iter(i)
+        try:
+            while True:
+                self.seen += 1
+                item = next(iterator)
+                if len(self.samples) < self.size:
+                    self.samples.append(item)
+                elif self.seen < gap_threshold:
+                    k = int(self.rng.random() * self.seen)
+                    if k < self.size:
+                        self.samples[k] = item
+                else:
+                    gap = int(math.log(self.rng.random()) / math.log(1 - self.size / self.seen))
+                    self.seen += gap
+                    for _ in range(gap):
+                        item = next(iterator)
+                    k = int(self.rng.random() * self.size)
+                    self.samples[k] = item
+        except StopIteration:
+            pass
